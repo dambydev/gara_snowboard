c3c386991afb1d624ca29cfd979874e86f07d903
Davide D'Ambrosio <davide431@outlook.it>
2023-02-23 13:37:03 +0100

    Programma finito
    Il programma è finito, bisogna solo inserire la documentazione

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@
 */
 #include <iostream>
 #include <fstream>
-#include <list>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+
 using namespace std;
 /*
  Traccia
@@ -18,99 +23,196 @@ using namespace std;
  (Le coordinate vanno generate tenendo conto di un piano cartesiano da 0,0 a 100,100)
  */
 
-/*struct competitor{
+struct competitor{
     int matricola, x, y, distance;
     string cog;
-};*/
-
-class competitor{
-    public:
-    int x, y, distance;
-    string cog, matricola;
 };
 
-competitor(){
-
+void write_original_file(vector<competitor> vet){
+    ofstream fout("list.txt");
+    fout<<"";
+        for(int i=0; i<vet.size(); i++){
+            fout<<vet.at(i).matricola<<"; ";
+            fout<<vet.at(i).cog<<"; ";
+            fout<<'0'<<"; ";
+            fout<<'0'<<"; ";
+            if(i != vet.size()-1)
+                fout<<'0'<<";"<<'\n';
+            else
+                fout<<'0'<<";";
+            }
 }
 
-void full_vet(list<competitor> vet){
-    ifstream fin("list.txt", ios::in);
+void write_file(vector<competitor> vet, vector<competitor> podio){
+    ofstream fout("list.txt");
+    fout<<"";
+    for(int i=0; i<podio.size(); i++){
+        fout<<podio.at(i).matricola<<"; ";
+        fout<<podio.at(i).cog<<"; ";
+        fout<<podio.at(i).x<<"; ";
+        fout<<podio.at(i).y<<"; ";
+        fout<<podio.at(i).distance<<";"<<'\n';
+    }
+    for(int i=0; i<vet.size(); i++){
+        fout<<vet.at(i).matricola<<"; ";
+        fout<<vet.at(i).cog<<"; ";
+        fout<<vet.at(i).x<<"; ";
+        fout<<vet.at(i).y<<"; ";
+        if(i != vet.size()-1)
+            fout<<vet.at(i).distance<<";"<<'\n';
+        else
+            fout<<vet.at(i).distance<<";";
+    }
+}
+
+void full_vet(vector<competitor>& vet){
+    ifstream fin("list.txt");
     if(!fin)
         cout<<"Errore di apertura del file";
     string app;
-    int q;
-    int i = 0;
+    int q = 0;
+    int i;
+    bool cont = false;
+
     while(!fin.eof()){
-        if(i != 4) {
-            competitor c;
+        competitor c;
+        i = 0;
+        cont = false;
+        while(!cont){
             getline(fin, app, ';');
             switch(i){
                 case 0:
-                    c.matricola = app;
+                    stringstream(app) >> q;
+                    c.matricola = q;
                     break;
                 case 1:
-                    app.erase
+                    app.erase(remove(app.begin(),app.end(),' '),app.end());
                     c.cog = app;
                     break;
                 case 2:
-                    q = stoi(app);
+                    app.erase(remove(app.begin(),app.end(),' '),app.end());
+                    stringstream(app) >> q;
                     c.x = q;
                     break;
                 case 3:
-                    q = stoi(app);
+                    app.erase(remove(app.begin(),app.end(),' '),app.end());
+                    stringstream(app) >> q;
                     c.y = q;
+                    break;
+                case 4:
+                    app.erase(remove(app.begin(),app.end(),' '),app.end());
+                    stringstream(app) >> q;
+                    c.distance = q;
+                    vet.push_back(c);
+                    cont = true;
                     break;
             }
             i++;
         }
+    }
+    vet.erase(vet.begin()+vet.size()-1);
+}
+
+int first(vector<competitor> vet){
+    int k;
+    for (int i = 0; i < vet.size(); i++){
+        if(i==0) {
+            if (vet.at(i).distance > vet.at(i + 1).distance)
+                k = i;
+            else
+                k = i+1;
+            i++;
+        }
         else{
-            getline(fin, app, ';');
+            if(vet.at(k).distance<vet.at(i).distance)
+                k = i;
+        }
+    }
+    return k;
+}
+
+void generate_position(vector<competitor>& vet){
+    for(int q = 0; q<30; q++) {
+        for (int i = 0; i < vet.size(); i++) {
+            int x1 = vet.at(i).x;
+            int y1 = vet.at(i).y;
+            vet.at(i).x = rand() % 101;
+            vet.at(i).y = rand() % 101;
+            vet.at(i).distance = vet.at(i).distance + sqrt((y1 - vet.at(i).y) * (y1 - vet.at(i).y) + (x1 - vet.at(i).x) * (x1 - vet.at(i).x));
         }
     }
 }
 
-void new_race(){
+void podium(vector<competitor> vet){
+    vector<competitor> podio;
+    for(int i=0; i<3; i++){
+        competitor c = vet.at(first(vet));
+        vet.erase(vet.begin()+first(vet));
+        podio.push_back(c);
+        cout<<endl<<"Al "<<i+1<<" posto abbiamo: "<<podio.at(i).cog<<" con una distanza percorsa di "<<podio.at(i).distance<<endl;
+    }
+    write_file(vet, podio);
+}
+
+void new_race(vector<competitor>& vet){
+    ifstream fin("list.txt");
+    if(!fin)
+        cout<<"Errore di apertura del file";
+    vet.clear();
+    full_vet(vet);
+    write_original_file(vet);
+    generate_position(vet);
+    cout<<endl<<"La gara e' iniziata!"<<endl;
+}
+
+void out_file(){
     ifstream fin("list.txt", ios::in);
     if(!fin)
         cout<<"Errore di apertura del file";
-    list<competitor> vet;
-    full_vet(vet);
-}
-
-void res_out(){
-
+    string l;
+    cout<<endl;
+    while(!fin.eof()){
+        getline(fin, l);
+        cout<<l<<endl;
+    }
 }
 
 void menu()
 {
     int sc;
+    vector<competitor> vet;
     do {
         cout << endl;
         cout << "------------------- MENU' -------------------" << endl;
         cout << "1 - Inizia gara nuova" << endl
              << "2 - Visualizza risultato" << endl
-             << "3 - Uscita" << endl << endl
+             << "3 - Stampa file" << endl
+             << "4 - Uscita" << endl << endl
              << "Inserire scelta >> ";
         cin >> sc;
         switch (sc)
         {
             case 1:
-                new_race();
+                new_race(vet);
                 break;
             case 2:
-                res_out();
+                podium(vet);
                 break;
             case 3:
+                out_file();
+                break;
+            case 4:
                 cout<<endl<<"Uscita..."<<endl;
                 break;
             default:
                 cout << endl << "Scelta sbagliata" << endl << endl;
         }
-    } while (sc != 3);
+    } while (sc != 4);
 }
 
 int main()
 {
+    srand(time(NULL));
     menu();
     return 0;
 }
